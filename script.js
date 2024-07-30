926c4c8b9383544810cd20309a48d3ff154f707e
nhatminhit <31946502+nhatminhit@users.noreply.github.com>
2024-07-30 21:30:48 +0700

    xoa cmt

--- script.js
+++ script.js
@@ -4,43 +4,6 @@ function playAudio() {
         audio.play();
     }
 }
-// function rollDice() {
-//     const studentName = prompt("Nhập tên sinh viên:");
-//     if (studentName) {
-//         let validInput = false;
-//         let userNumber;
-        
-//         while (!validInput) {
-//             const numberInput = prompt("Nhập một số trong khoảng từ 1 đến 40:");
-//             userNumber = parseInt(numberInput, 10);
-            
-//             if (!isNaN(userNumber) && userNumber >= 1 && userNumber <= 40) {
-//                 validInput = true;
-//             } else {
-//                 alert("Vui lòng nhập một số hợp lệ trong khoảng từ 1 đến 40.");
-//             }
-//         }
-
-//         const dice = [userNumber];
-//         while (dice.length < 3) {
-//             const randomNum = Math.floor(Math.random() * 40) + 1;
-//             if (!dice.includes(randomNum)) {
-//                 dice.push(randomNum);
-//             }
-//         }
-
-//         playAudio();
-//         document.getElementById('dice1').textContent = dice[0];
-//         document.getElementById('dice2').textContent = dice[1];
-//         document.getElementById('dice3').textContent = dice[2];
-
-//         const resultDiv = document.getElementById('result');
-//         resultDiv.textContent = `Kết quả: ${dice.join(", ")}`;
-//         resultDiv.style.display = 'block'; // Hiển thị phần kết quả
-
-//         saveResult(studentName, dice);
-//     }
-// }
 function rollDice() {
     const studentName = prompt("Nhập tên sinh viên:");
     if (studentName) {
